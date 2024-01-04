31678c510c0801fab0a309eff464a307e8c8771c
Andre Lusegardis <lusegardis@gmail.com>
2024-01-03 21:34:59 -0300

    portfolio done

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,10 +1,51 @@
+import { ReactNode, useMemo } from "react";
+import * as Projects from "./projects";
+import Image from "next/image";
 import styles from "./portfolio.module.css";
+import Link from "next/link";
+import { RiCodeSSlashLine } from "react-icons/ri";
 
 const Portfolio = () => {
+  const automaticHtml = useMemo((): ReactNode[] => {
+    return Projects.projects.map((item) => (
+      <Link
+        key={item}
+        href={`https://github.com/MestreALMO/` + item}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${styles.portfolioCard}`}
+      >
+        <figure className={`${styles.figure}`}>
+          <Image
+            src={
+              `${Projects.githubLink}` +
+              `${item}` +
+              "/blob/main/_README.md/img01.png?raw=true"
+            }
+            alt={item}
+            width="1919"
+            height="1079"
+            layout="responsive"
+            objectFit="contain"
+            className={`${styles.image}`}
+          />
+          <div className={`${styles.figureContent}`}>
+            <RiCodeSSlashLine />
+            <figcaption>{item}</figcaption>
+          </div>
+        </figure>
+      </Link>
+    ));
+  }, []);
+
   return (
     <>
       <div className={`${styles.main}`}>
         <h1 className={`${styles.mainTitle}`}>Portfolio</h1>
+        <p className={`${styles.warning}`}>
+          Click on any project to know more about it!
+        </p>
+        <div className={`${styles.content}`}>{automaticHtml}</div>
       </div>
     </>
   );
---
+++ src/pages/portfolio/projects.ts
@@ -0,0 +1,49 @@
+export const githubApi = "https://api.github.com/repos/MestreALMO/";
+export const githubLink = "https://github.com/MestreALMO/";
+
+const arrayProjects = [
+  "nata.house-challenge",
+  "portfolio",
+  "MirageJS-Test",
+  "Edusynch-Challenge",
+  "Using-Studio-Ghilis-API-With-JS-Only",
+  "my-portfolio-next",
+  "Youtube-Navbar-2021",
+  "Mana-raWozonWebsite",
+  "Movies-Website",
+  "Add-Remove-Mark-and-Mark-off-With-ReactJS",
+  "My-Portfolio",
+  "Github_Explorer",
+  "MestreALMO.github.io",
+  "Tests-With-useState-useEffect-useRef",
+  "Tic-Tac-Toe-React-in-JS",
+  // "ReactJS-with-TypeScript-Template",
+  // "Retractable-Accordion",
+];
+
+export const arrayProjectsInfo = [
+  ["nata.house-challenge", "Testing Star Wars API"],
+  ["portfolio", "My portfolio of 2021"],
+  ["MirageJS-Test", "Understanding MirageJS, a fake API"],
+  ["Edusynch-Challenge", "Cloning a website challenge"],
+  ["Using-Studio-Ghilis-API-With-JS-Only", "Grabing API data"],
+  ["my-portfolio-next", "My 2021 portfolio rebuilt with Nextjs"],
+  ["Youtube-Navbar-2021", "A build of YouTube's Topbar"],
+  ["Mana-raWozonWebsite", "A portfolio that I gave to a friend"],
+  ["Movies-Website", "A movies website"],
+  [
+    "Add-Remove-Mark-and-Mark-off-With-ReactJS",
+    "Testing Array of object in React",
+  ],
+  ["My-Portfolio", "My first portfolio of all times"],
+  ["Github_Explorer", "This is a explorer that uses real data from GitHub"],
+  [
+    "MestreALMO.github.io",
+    "A version of my firstportfolio that's online on Github",
+  ],
+  ["Tests-With-useState-useEffect-useRef", "Learning the hooks in the title"],
+  ["Tic-Tac-Toe-React-in-JS", "A tic-tac-toe game built using React"],
+];
+// arrayProjects.length % 2 !== 0 &&
+//   arrayProjects.splice(arrayProjects.length - 1, 1);
+export const projects = arrayProjects;
